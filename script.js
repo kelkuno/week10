66fa36e9da53d9b9e787773e218af632528b490f
Kelsey Kuno <kelsey.kuno@gmail.com>
2019-04-07 11:32:20 -0500

    assignment completed

--- script.js
+++ script.js
@@ -3,3 +3,34 @@ Assignent 10
 
 ______________________________*/
 
+var housePlants = [
+    {
+        name: "pothos",
+        age: 7,
+        care: "water when plant looks less perky"
+    }, {
+        name: "money tree",
+        age: 2,
+        care: "water when leaves begin to wilt"
+    }, {
+        name: "aloe",
+        age: 6,
+        care: "water when arms get squishy"
+    }, {
+        name: "christmas cactus",
+        age: 3,
+        care: "water when you feel like it. also use fertilizer"
+    }, {
+        name: "ZZ plant",
+        age: 4,
+        care: "water when you feel like it but don't feel like it too often."
+    }
+];
+
+var myVue = new Vue({
+    el: "#myApp",
+    data: {
+      plants: housePlants
+    }
+  });
+  
