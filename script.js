f5b48e5bc159bbf5b29ad86ac5ac5f4f75581487
Kelsey Kuno <kelsey.kuno@gmail.com>
2019-04-14 10:39:50 -0500

    Delete button enabled

--- script.js
+++ script.js
@@ -7,23 +7,28 @@ var housePlants = [
     {
         name: "pothos",
         age: 7,
-        care: "water when plant looks less perky"
+        care: "water when plant looks less perky",
+        id: 11
     }, {
         name: "money tree",
         age: 2,
-        care: "water when leaves begin to wilt"
+        care: "water when leaves begin to wilt",
+        id: 22
     }, {
         name: "aloe",
         age: 6,
-        care: "water when arms get squishy"
+        care: "water when arms get squishy",
+        id: 33
     }, {
         name: "christmas cactus",
         age: 3,
-        care: "water when you feel like it. also use fertilizer"
+        care: "water when you feel like it. also use fertilizer",
+        id: 44
     }, {
         name: "ZZ plant",
         age: 4,
-        care: "water when you feel like it but don't feel like it too often."
+        care: "water when you feel like it but don't feel like it too often.",
+        id: 55
     }
 ];
 
@@ -31,6 +36,18 @@ var myVue = new Vue({
     el: "#myApp",
     data: {
       plants: housePlants
+    },
+    methods: {
+        deletePlant: function (plantObject){
+            this.plants = this.plants.filter(function 
+                (plant){
+                if (plant.id !== plantObject.id) {
+                    return true;
+                } else {
+                return false;
+                }
+            });
+        }
     }
-  });
+    });
   
