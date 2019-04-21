31a8bad4904238f0cc116a3146523ceb12361026
Kelsey Kuno <kelsey.kuno@gmail.com>
2019-04-21 16:09:37 -0500

    photos complete

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ Vue.component('plants-component', {
     props: ['name', 'age', 'care', 'id', 'pic'],
     template: `
         <tr>
-            <td>{{pic}}</td>
+            <td><img v-bind:src="pic" v-bind:alt="name"></td>
             <td>{{name}}</td>
             <td>{{age}}</td>
             <td>{{care}}</td>
@@ -22,27 +22,31 @@ var housePlants = [
         age: 7,
         care: "water when plant looks less perky",
         id: 11,
-        pic: "pics/pothos.jpeg"
+        pic: "pics/pothos_250.jpeg"
     }, {
         name: "money tree",
         age: 2,
         care: "water when leaves begin to wilt",
-        id: 22
+        id: 22,
+        pic: "pics/money-tree_250.jpeg"
     }, {
         name: "aloe",
         age: 6,
         care: "water when arms get squishy",
-        id: 33
+        id: 33,
+        pic: "pics/aloe_250.jpeg"
     }, {
         name: "christmas cactus",
         age: 3,
         care: "water when you feel like it. also use fertilizer",
-        id: 44
+        id: 44,
+        pic: "pics/christmas-cactus_250.jpeg"
     }, {
         name: "ZZ plant",
         age: 4,
         care: "water when you feel like it but don't feel like it too often.",
-        id: 55
+        id: 55,
+        pic: "pics/zz_250.jpeg"
     }
 ];
 
